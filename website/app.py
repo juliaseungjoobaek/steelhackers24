19e52a3c67dadf52e46774befa9d82bb109d10ef
Seungjoo Baek <136966419+juliaseungjoobaek@users.noreply.github.com>
2024-09-29 11:55:28 -0400

    Add files via upload

---
+++ website/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, redirect, url_for
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    dish = request.form.get('dish')
+    print(f"Dish received in app.py: {dish}")
+    
+    result = subprocess.run(['python3', '/home/yoojin.baek/steelhackers/src/main.py', dish], capture_output=True, text=True)
+    
+    # main.py의 처리 결과를 출력
+    output = result.stdout
+    
+    return f"Dish received and processed by main.py: {output}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
