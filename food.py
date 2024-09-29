fd4936f047b70587c22f10e24173b8cfffa973b2
nickyhsc <92796622+nickyhsc@users.noreply.github.com>
2024-09-29 08:27:13 -0400

    Update food.py

--- food.py
+++ food.py
@@ -1,27 +1,21 @@
-from flask import Flask, request, redirect, url_for, render_template
 import json
 
-app = Flask(__name__)
+# Attempt to read the nutrition data
 
-# Load nutrition data
 with open('nutrition.json', 'r') as file:
     nutrition = json.load(file)
 
 total_Calories = total_Protein = total_Carbohydrates = total_Sugar = total_Fat = total_DietaryFiber = total_Sodium = 0
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+keep_choosing = True
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    global total_Calories, total_Protein, total_Carbohydrates, total_Sugar, total_Fat, total_DietaryFiber, total_Sodium
-    restaurant = request.form['place']
-    food = request.form['dish']  # Assuming you have a default food item
-
+while keep_choosing:
+    restaurant = input("Which restaurant did you went? ")
+    food = input("What food item did you get? ")
+    
     # Access the nutrition information
     what = nutrition[restaurant][food]
-   
+    
     total_Calories += what['Calories']
     total_Protein += what['Protein']
     total_Carbohydrates += what['Carbohydrates']
@@ -30,20 +24,16 @@ def submit():
     total_DietaryFiber += what['Dietary Fiber']
     total_Sodium += what['Sodium']
 
-    return redirect(url_for('result'))
+    done_or_not = input("Enter 'yes' if you are done choosing or 'no' if you still want to add food to your cart: ")
+    if done_or_not.lower() == "yes":
+        keep_choosing = False
 
-@app.route('/result')
-def result():
-    global total_Calories, total_Protein, total_Carbohydrates, total_Sugar, total_Fat, total_DietaryFiber, total_Sodium
-    return (f"Total calories served: {total_Calories} (cal)<br>"
-            f"Total protein served: {total_Protein} (g)<br>"
-            f"Total carbohydrates served: {total_Carbohydrates} (g)<br>"
-            f"Total sugar served: {total_Sugar} (g)<br>"
-            f"Total fat served: {total_Fat} (g)<br>"
-            f"Total dietary fiber served: {total_DietaryFiber} (g)<br>"
-            f"Total sodium served: {total_Sodium} (mg)")
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
+    
+# Print total nutrition information
+print("Total calories served:", total_Calories, "(cal)")
+print("Total protein served:", total_Protein, "(g)")
+print("Total carbohydrates served:", total_Carbohydrates, "(g)")
+print("Total sugar served:", total_Sugar, "(g)")
+print("Total fat served:", total_Fat, "(g)")
+print("Total dietary fiber served:", total_DietaryFiber, "(g)")
+print("Total sodium served:", total_Sodium, "(mg)")
